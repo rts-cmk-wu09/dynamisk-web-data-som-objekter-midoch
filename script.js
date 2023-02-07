fe8bc1eb5aadcc70fe0fe7ff0b0bb225085d51ad
midoch <101448145+midoch@users.noreply.github.com>
2023-02-07 12:05:28 +0100

    Solved Opgave 2.

--- script.js
+++ script.js
@@ -31,3 +31,31 @@ console.log(person["firstname"]);
 console.log(person["hobbies"][1]);
 
 console.log(person["pet"]["name"]);
+
+// -- Opgave 2 --
+
+let car = {
+  name: "BMW",
+  model: "M3",
+  isNew: true,
+  color: ["Black", "White", "Red"],
+  hometown: "Copenhagen",
+  wheelSize: {
+    size1: "small",
+    size2: "medium",
+    size3: "large",
+  },
+};
+
+console.log(
+  car.name,
+  car.model,
+  car.isNew,
+  car.color[1],
+  car.hometown,
+  car.wheelSize.size2
+);
+
+car.color.forEach(function (color) {
+  console.log("This is color:", color);
+});
