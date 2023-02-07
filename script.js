3eae813e0e023ee27634cfc8d7e6cf633a687527
midoch <101448145+midoch@users.noreply.github.com>
2023-02-07 11:14:28 +0100

    Create script.js
    For solving the assignment

---
+++ script.js
@@ -0,0 +1,21 @@
+// -- Opgave 1 --
+
+let person = {
+  firstname: "Ch",
+  lastname: "Hc",
+  age: 23,
+  hometown: "Copenhagen",
+  pet: "Cat",
+  petname: "Tommy",
+};
+
+console.log(
+  person.firstname,
+  person.lastname,
+  person.age,
+  person.hometown,
+  person.pet,
+  person.petname
+);
+
+console.log(person["firstname"]);
