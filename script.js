e268bac5b131ada73f8e629d8901811b20c74b51
midoch <101448145+midoch@users.noreply.github.com>
2023-02-07 11:22:04 +0100

    Solved Opgave 1.

--- script.js
+++ script.js
@@ -3,19 +3,31 @@
 let person = {
   firstname: "Ch",
   lastname: "Hc",
+  married: false,
   age: 23,
   hometown: "Copenhagen",
-  pet: "Cat",
-  petname: "Tommy",
+  hobbies: ["Gaming", "Investing", "Programming"],
+  pet: {
+    type: "Cat",
+    name: "Tommy",
+  },
 };
+
+// Console logging
 
 console.log(
   person.firstname,
   person.lastname,
+  person.married,
   person.age,
   person.hometown,
-  person.pet,
-  person.petname
+  person.hobbies,
+  person.pet.type,
+  person.pet.name
 );
 
 console.log(person["firstname"]);
+
+console.log(person["hobbies"][1]);
+
+console.log(person["pet"]["name"]);
