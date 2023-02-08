3cfefff19e00ff32e3c8ac2811758e92abdfd383
midoch <101448145+midoch@users.noreply.github.com>
2023-02-08 12:51:16 +0100

    Update data.js

---
+++ scripts/data.js
@@ -0,0 +1,29 @@
+let person = {
+  persondata: {
+    extra: {
+      køn: "Mand",
+      cpr_nummer: "250590-3575",
+      fødselsdato: "25.05.1990",
+      civilstand: "Single",
+      børn: "Ingen",
+    },
+
+    navne: {
+      fornavn: "Hans",
+      efternavn: "Hansen",
+    },
+
+    adresse: {
+      vejnavn: "Hovedgaden",
+      husnummer: "25",
+      postnummer: "3550",
+      by: "København",
+    },
+
+    kontakt: {
+      telefonnummer: "30-75-20-22",
+      email: "HansHansen@gmail.com",
+      hjemmeside: "www.hanshansen.dk",
+    },
+  },
+};
