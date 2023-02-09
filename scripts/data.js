86d491fa49ab05beb192c2303384548c4ffa66ea
midoch <101448145+midoch@users.noreply.github.com>
2023-02-09 14:10:42 +0100

    created a new display metode

--- scripts/data.js
+++ scripts/data.js
@@ -1,29 +1,60 @@
-let person = {
-  persondata: {
-    extra: {
-      køn: "Mand",
-      cpr_nummer: "250590-3575",
-      fødselsdato: "25.05.1990",
-      civilstand: "Single",
-      børn: "Ingen",
-    },
+let person = [
+  {
+    persondata: {
+      navne: {
+        fornavn: "Ch",
+        efternavn: "Jackson",
+      },
 
-    navne: {
-      fornavn: "Ch",
-      efternavn: "Jackson",
-    },
+      adresse: {
+        vejnavn: "Hovedbanegårdspladsen",
+        husnummer: "11",
+        postnummer: "2250",
+        by: "København",
+      },
 
-    adresse: {
-      vejnavn: "Hovedbanegårdspladsen",
-      husnummer: "11",
-      postnummer: "2250",
-      by: "København",
-    },
+      kontakt: {
+        telefonnummer: "+45 30 75 20 22",
+        email: "JacksonTech@gmail.com",
+        hjemmeside: "www.JacksonTechLab.dk",
+      },
 
-    kontakt: {
-      telefonnummer: "+45 30 75 20 22",
-      email: "JacksonTech@gmail.com",
-      hjemmeside: "www.JacksonTechLab.dk",
+      extra: {
+        køn: "Mand",
+        cpr_nummer: "250590-3575",
+        fødselsdato: "25.05.1990",
+        civilstand: "Single",
+        børn: "Ingen",
+      },
     },
   },
-};
+  {
+    persondata: {
+      navne: {
+        fornavn: "Emma",
+        efternavn: "Smith",
+      },
+
+      adresse: {
+        vejnavn: "Roskildevej",
+        husnummer: "27",
+        postnummer: "2500",
+        by: "Valby",
+      },
+
+      kontakt: {
+        telefonnummer: "+45 31 32 33 34",
+        email: "emma.smith@gmail.com",
+        hjemmeside: "www.emmasmith.dk",
+      },
+
+      extra: {
+        køn: "Kvinde",
+        cpr_nummer: "300690-2275",
+        fødselsdato: "30.06.1990",
+        civilstand: "Gift",
+        børn: "2",
+      },
+    },
+  },
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,56 +1,58 @@
-document.querySelector(".person__info").innerHTML = `
+person.forEach((item) => {
+  document.querySelector(".person__info").innerHTML += `
 
-  <section>
-  <p> <span class="p__element">Fornavn:</span>
-  ${person.persondata.navne.fornavn}
-  </#p>
-  <p> <span class="p__element">Efternavn:</span> 
-  ${person.persondata.navne.efternavn}
-  </p>
+    <section>
+    <p> <span class="p__element">Fornavn:</span>
+    ${item.persondata.navne.fornavn}
+    </#p>
+    <p> <span class="p__element">Efternavn:</span> 
+    ${item.persondata.navne.efternavn}
+    </p>
 
-  <br>
+    <br>
 
-  <p> <span class="p__element">Køn:</span> 
-  ${person.persondata.extra.køn}
-  </p>
-  <p> <span class="p__element">CPR-Nummer:</span> 
-  ${person.persondata.extra.cpr_nummer}
-  </p>
-  <p> <span class="p__element">Fødselsdato:</span> 
-  ${person.persondata.extra.fødselsdato}
-  </p>
-  <p> <span class="p__element">Civilstand:</span>  
-  ${person.persondata.extra.civilstand}
-  </p>
-  <p> <span class="p__element">Børn:</span> 
-  ${person.persondata.extra.børn}
-  </p>
+    <p> <span class="p__element">Køn:</span> 
+    ${item.persondata.extra.køn}
+    </p>
+    <p> <span class="p__element">CPR-Nummer:</span> 
+    ${item.persondata.extra.cpr_nummer}
+    </p>
+    <p> <span class="p__element">Fødselsdato:</span> 
+    ${item.persondata.extra.fødselsdato}
+    </p>
+    <p> <span class="p__element">Civilstand:</span>  
+    ${item.persondata.extra.civilstand}
+    </p>
+    <p> <span class="p__element">Børn:</span> 
+    ${item.persondata.extra.børn}
+    </p>
 
-  <br>
+    <br>
 
-  <p> <span class="p__element">Vejnavn:</span>
-  ${person.persondata.adresse.vejnavn}
-  </p>
-  <p> <span class="p__element">Husnummer:</span>
-  ${person.persondata.adresse.husnummer}
-  </p>
-  <p> <span class="p__element">Postnummer:</span>
-  ${person.persondata.adresse.postnummer}
-  </p>
-  <p> <span class="p__element">By:</span> 
-  ${person.persondata.adresse.by}
-  </p>
+    <p> <span class="p__element">Vejnavn:</span>
+    ${item.persondata.adresse.vejnavn}
+    </p>
+    <p> <span class="p__element">Husnummer:</span>
+    ${item.persondata.adresse.husnummer}
+    </p>
+    <p> <span class="p__element">Postnummer:</span>
+    ${item.persondata.adresse.postnummer}
+    </p>
+    <p> <span class="p__element">By:</span> 
+    ${item.persondata.adresse.by}
+    </p>
 
-  <br>
+    <br>
 
-  <p> <span class="p__element">Telefonnummer:</span> 
-  ${person.persondata.kontakt.telefonnummer}
-  </p>
-  <p> <span class="p__element">Email:</span> 
-  ${person.persondata.kontakt.email}
-  </p>
-  <p> <span class="p__element">Hjemmeside:</span> 
-  ${person.persondata.kontakt.hjemmeside}
-  </p>
-  </section>
-`;
+    <p> <span class="p__element">Telefonnummer:</span> 
+    ${item.persondata.kontakt.telefonnummer}
+    </p>
+    <p> <span class="p__element">Email:</span> 
+    ${item.persondata.kontakt.email}
+    </p>
+    <p> <span class="p__element">Hjemmeside:</span> 
+    ${item.persondata.kontakt.hjemmeside}
+    </p>
+    </section>
+  `;
+});
